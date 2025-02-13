33394a534ce40bd133982a09fe04cd3ab6a3835f
Mohaideen Abdul Kathar S <157392970+Mohaideen-Abdul-Kathar-S@users.noreply.github.com>
2025-02-13 10:29:54 +0530

    Delete tut directory

--- tut/src/main/java/com/example/tut/repository/CklRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tut.repository;
-
-import com.example.tut.model.Clk;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CklRepository extends MongoRepository<Clk,Integer> {
-    
-}
